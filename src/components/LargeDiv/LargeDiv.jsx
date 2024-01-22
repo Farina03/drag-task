b955d0ed5094fe903250b4061ad0f40b555951bb
Farina Tahsin Chowdhury <farina@Farinas-MacBook-Air.local>
2024-01-22 17:10:57 +0600

    resizing on four sides

--- src/components/LargeDiv/LargeDiv.jsx
+++ src/components/LargeDiv/LargeDiv.jsx
@@ -4,57 +4,197 @@ import './largediv.css'
 import { createPortal } from 'react-dom';
 
 const LargeDiv = () => {
-  let toSubFromX = 0
-  let toSubFromY = 0
+  const largeDivWidth = 300  
+  const smallDivWidth = 50
+  const tooltipHeight = 30
+  const tooltipWidth = 100
+  const optionsWidth = 150
   const [position, setPosition] = useState({ x: 0, y: 0});
   const [isDragging, setIsDragging] = useState(false);
   const [tooltipPosition, setTooltipPosition] = useState({x:0, y:0})
   const [tooltipVisible, setTooltipVisible] = useState(false)
-  const [largeDivPos, setLargeDivPos] = useState({x: 0, y: 0})
+  const [largeDivPos, setLargeDivPos] = useState(
+    {x: (window.innerWidth/2)-largeDivWidth/2, 
+     y: (window.innerHeight/2)-largeDivWidth})
+  const [largeDivIsDragging, setLargeDivIsDragging] = useState(false)
+  const [largeDivDimensions, setLargeDivDimensions] = useState({height: 300, width: 300})
+  const [isResizing, setIsResizing] = useState({resize:false, direction:""})
   const [options, setOptions] = useState("")
   const largeDivRef = useRef(null)
-  
-   useEffect(() => {
-    toSubFromX = largeDivRef?.current?.getBoundingClientRect().left
-    toSubFromY = largeDivRef?.current?.getBoundingClientRect().top
-    setPosition({
-      x: toSubFromX,
-      y: toSubFromY
-    })
-    console.log(toSubFromX, toSubFromY, "sub")
-    // setLargeDivPos({x: largeDivPos.x - toSubFromX,
-    //                 y: largeDivPos.y - toSubFromY})
-   }, [])
+  const smallDivRef = useRef(null)
 
-    const handleMouseOut = (e) => {
-        // setIsDragging(false)
-        setTooltipVisible(false)
+  const largeDivStyle = {
+    left: largeDivPos.x,
+    top: largeDivPos.y,
+    height: largeDivDimensions.height,
+    width: largeDivDimensions.width
+  }
+  const tooltipStyle = {
+    visibility: tooltipVisible ? "visible" : "hidden",
+    position: "fixed",
+    left: tooltipPosition.x,
+    top: tooltipPosition.y,
+    border: ".5px solid black",
+    height: "30px",
+    width: "100px", //150
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    fontSize: "12px"
+  }
+  const optionsStyle = {
+    marginLeft: window.innerWidth/2 - optionsWidth/2,
+    marginTop: "50px"
+  }
+  function handleOptionChange(e) {
+    setOptions(e.target.value)
+  }
+  function handleDragBtnMouseDown() {
+    setLargeDivIsDragging(true)
+  }
+  function handleMainDivMouseMove(e) {        //write the resizing conditions
+    if(largeDivIsDragging) {
+      setTooltipVisible(false)
+      const newX = largeDivPos.x + e.movementX
+      const newY = largeDivPos.y + e.movementY
+      setLargeDivPos({
+        x: Math.max(0, Math.min(newX, window.innerWidth-largeDivDimensions.width)),
+        y: Math.max(0-80, Math.min(newY, window.innerHeight-55-largeDivDimensions.height))
+      })
     }
-    // console.log(largeDivRef?.current?.getBoundingClientRect().left + 150)
-    const largeDivStyle = {
-      left: largeDivPos.x,
-      top: largeDivPos.y
+    if(isDragging) {
+      setTooltipVisible(false)
+      const newX = position.x + e.movementX
+      const newY = position.y + e.movementY
+      const maxX = largeDivRef.current.getBoundingClientRect().width-smallDivWidth
+      const maxY = largeDivRef.current.getBoundingClientRect().height-smallDivWidth
+      const boundX = Math.max(0, Math.min(newX, maxX))   
+      const boundY = Math.max(0, Math.min(newY, maxY))  
+      setPosition({ x: boundX, y: boundY });
     }
-    const tooltipStyle = {
-        visibility: tooltipVisible ? "visible" : "hidden",
-        position: "fixed",
-        left: tooltipPosition.x,
-        top: tooltipPosition.y,
-        border: ".5px solid black",
-        height: "30px",
-        width: "100px", //150
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        fontSize: "12px"
+    if(isResizing.resize === true) {
+      if(isResizing.direction === "left") {
+        if(position.x+smallDivWidth >= largeDivDimensions.width) {
+          setPosition(prevItem => ({
+            ...prevItem,
+            x: Math.max(0,prevItem.x-e.movementX)
+          }))
+        }
+        if(largeDivDimensions.width > smallDivWidth) {
+          setLargeDivPos(prevItem => ({
+            ...prevItem,
+            x: prevItem.x+e.movementX
+          }))
+        }
+        setLargeDivDimensions(prevItem => (
+          {
+            ...prevItem,
+            width: Math.max(smallDivWidth, prevItem.width - e.movementX)
+          }
+        ))
+      }
+      if(isResizing.direction === "right") {
+        if(position.x+smallDivWidth >= largeDivDimensions.width) {
+          setPosition(prevItem => ({
+            ...prevItem,
+            x: Math.max(0, prevItem.x+e.movementX)
+          }))
+        }
+        setLargeDivDimensions(prevItem => (
+          {
+            ...prevItem,
+            width: Math.max(smallDivWidth, prevItem.width + e.movementX)
+          }
+        ))
+      }
+      if(isResizing.direction === "top") {
+        if(position.y+smallDivWidth >= largeDivDimensions.height) {
+          setPosition(prevItem => ({
+            ...prevItem,
+            y: Math.max(0,prevItem.y-e.movementY)
+          }))
+        }
+        if(largeDivDimensions.height > smallDivWidth) {
+          setLargeDivPos(prevItem => ({
+            ...prevItem,
+            y: largeDivPos.y+e.movementY
+          }))
+        }
+        setLargeDivDimensions(prevItem => (
+          {
+            ...prevItem,
+            height: Math.max(smallDivWidth, prevItem.height - e.movementY)
+          }
+        ))
+      }
+      if(isResizing.direction === "bottom") {
+        if(position.y+smallDivWidth >= largeDivDimensions.height) {
+          setPosition(prevItem => ({
+            ...prevItem,
+            y: Math.max(0,prevItem.y+e.movementY)
+          }))
+        }
+        setLargeDivDimensions(prevItem => (
+          {
+            ...prevItem,
+            height: Math.max(smallDivWidth, prevItem.height + e.movementY)
+          }
+        ))
+      }
     }
-    function handleOptionChange(e) {
-      setOptions(e.target.value)
+  }
+  function handleMainDivMouseDown(e) { 
+    if(largeDivIsDragging) {
+      setLargeDivIsDragging(false)
     }
-    // console.log(smallDivRef.current?.getBoundingClientRect, "divRef")
+    if(isDragging) {
+      setIsDragging(false)
+    }
+    if(e.clientX >= largeDivRef.current.getBoundingClientRect().left-5 && 
+      e.clientX <= largeDivRef.current.getBoundingClientRect().left+5) {
+      setIsResizing({
+        resize: true,
+        direction: "left"
+      })
+    }
+    if(e.clientX >= largeDivRef.current.getBoundingClientRect().right-5 && 
+      e.clientX <= largeDivRef.current.getBoundingClientRect().right+5) {
+      setIsResizing({
+        resize: true,
+        direction: "right"
+      })
+    }
+    if(e.clientY >= largeDivRef.current.getBoundingClientRect().top-5 && 
+      e.clientY <= largeDivRef.current.getBoundingClientRect().top+5) {
+      setIsResizing({
+        resize: true,
+        direction: "top"
+      })
+    }
+    if(e.clientY >= largeDivRef.current.getBoundingClientRect().bottom-5 && 
+      e.clientY <= largeDivRef.current.getBoundingClientRect().bottom+5) {
+      setIsResizing({
+        resize: true,
+        direction: "bottom"
+      })
+    }
+  }
+  function handleMainDivMouseUp() { 
+    if(isDragging)
+      setIsDragging(false)
+    if(largeDivIsDragging)
+      setLargeDivIsDragging(false)
+    setIsResizing({
+      resize: false,
+      direction: ""
+    })
+  }
   return (
-    <div className="main-div">
-      <select className="select-options" value={options} onChange={handleOptionChange}>
+    <div className="main-div" 
+         onMouseMove={handleMainDivMouseMove} 
+         onMouseDown={handleMainDivMouseDown}
+         onMouseUp={handleMainDivMouseUp}>
+      <select className="select-options" value={options} style={optionsStyle} onChange={handleOptionChange}>
         <option value="">Select Direction</option>
         <option value="top">Top</option>
         <option value="bottom">Bottom</option>
@@ -62,14 +202,15 @@ const LargeDiv = () => {
         <option value="right">Right</option>
       </select>
       <div style={largeDivStyle} id="large-div" ref={largeDivRef} 
-          onMouseOut={handleMouseOut} 
-          className='large-div'>
-        <div className='large-div-drag'>
+            className='large-div'>
+        <div className='large-div-drag'
+             onMouseDown={handleDragBtnMouseDown}>
           Drag
         </div>
         {createPortal(
             <div className='tooltip' style={tooltipStyle}>This is tooltip</div>, document.body
         )}
+        <div ref={smallDivRef}>
         <SmallDiv isDragging={isDragging} 
                 setIsDragging={setIsDragging}
                 tooltipVisible={tooltipVisible}
@@ -79,10 +220,11 @@ const LargeDiv = () => {
                 setTooltipPosition={setTooltipPosition}
                 tooltipPosition={tooltipPosition}
                 largeDivRef={largeDivRef}
-                options={options}/>
-    </div>
-    </div>
-    
+                options={options}
+                largeDivPos={largeDivPos}/>
+        </div>
+      </div>
+    </div> 
   )
 }
 
--- src/components/SmallDiv/SmallDiv.jsx
+++ src/components/SmallDiv/SmallDiv.jsx
@@ -12,124 +12,107 @@ const SmallDiv = ({isDragging,
                 tooltipPosition,
                 options,
                 largeDivRef}) => {
-    // const [position, setPosition] = useState({ x: 0, y: 0 });
-    // const [tooltipPosition, setTooltipPosition] = useState({x: 0, y:0})
-    const smallDivRef = useRef(null)
-    const largeDivWidth = 300
-    const smallDivWidth = 50
-    const tooltipHeight = 30
-    const tooltipWidth = 100
-    const handleMouseDown = (e) => {
-      setIsDragging(true);
+  const smallDivRef = useRef(null)
+  const largeDivWidth = 300
+  const smallDivWidth = 50
+  const tooltipHeight = 30
+  const tooltipWidth = 100
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    setTooltipVisible(false)
+  };
+  
+  const handleMouseUp = (e) => {
+    setIsDragging(false);
+    let smallDivBottom = smallDivRef.current?.getBoundingClientRect().bottom
+    let smallDivTop = smallDivRef.current?.getBoundingClientRect().top
+    let smallDivLeft = smallDivRef.current?.getBoundingClientRect().left
+    let smallDivRight = smallDivRef.current?.getBoundingClientRect().right
+    let largeDivRight = largeDivRef.current?.getBoundingClientRect().right
+    let largeDivLeft = largeDivRef.current?.getBoundingClientRect().left
+    let largeDivBottom = largeDivRef.current?.getBoundingClientRect().bottom
+    let largeDivTop = largeDivRef.current?.getBoundingClientRect().top
+    if(options === "bottom") {
+      setTooltipPosition({
+        x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
+        y: smallDivBottom + tooltipHeight >= largeDivBottom ? 
+          smallDivBottom - (smallDivWidth + tooltipHeight) : 
+          smallDivBottom 
+      })
+    }
+    if(options === "top" || options === "") {
+      setTooltipPosition({
+        x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
+        y: smallDivTop - tooltipHeight <= largeDivTop ? 
+           smallDivBottom : smallDivTop - tooltipHeight
+      })
+    }
+    if(options === "right") {
+      setTooltipPosition({
+        x: smallDivRight+tooltipWidth >= largeDivRight ?
+            smallDivLeft-tooltipWidth : smallDivRight,
+          //Math.min(smallDivRight,largeDivRight-tooltipWidth),
+        y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
+      })
+    }
+    if(options === "left") {
+      setTooltipPosition({
+        x: smallDivLeft - tooltipWidth <= largeDivLeft ?
+            smallDivRight : smallDivLeft - tooltipWidth,
+        y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
+      })
+    }
+    setTooltipVisible(true)
+  };
+  const handleMouseEnter = (e) => {
+    let smallDivBottom = smallDivRef.current?.getBoundingClientRect().bottom
+    let smallDivTop = smallDivRef.current?.getBoundingClientRect().top
+    let smallDivLeft = smallDivRef.current?.getBoundingClientRect().left
+    let smallDivRight = smallDivRef.current?.getBoundingClientRect().right
+    let largeDivRight = largeDivRef.current?.getBoundingClientRect().right
+    let largeDivLeft = largeDivRef.current?.getBoundingClientRect().left
+    let largeDivBottom = largeDivRef.current?.getBoundingClientRect().bottom
+    let largeDivTop = largeDivRef.current?.getBoundingClientRect().top
+    if(options === "bottom") {
+      setTooltipPosition({
+        x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
+        y: smallDivBottom + tooltipHeight >= largeDivBottom ? 
+            smallDivBottom - (smallDivWidth + tooltipHeight) : 
+            smallDivBottom 
+      })
+    }
+    if(options === "top" || options === "") {
+      setTooltipPosition({
+        x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
+        y: smallDivTop - tooltipHeight <= largeDivTop ? 
+           smallDivBottom : smallDivTop - tooltipHeight
+      })
+    }
+    if(options === "right") {
+      setTooltipPosition({
+        x: smallDivRight+tooltipWidth >= largeDivRight ?
+            smallDivLeft-tooltipWidth : smallDivRight,
+          //Math.min(smallDivRight,largeDivRight-tooltipWidth),
+        y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
+      })
+    }
+    if(options === "left") {
+      setTooltipPosition({
+        x: smallDivLeft - tooltipWidth <= largeDivLeft ?
+           smallDivRight : smallDivLeft - tooltipWidth,
+        y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
+      })
+    }
+    setTooltipVisible(true)
+  }
+  const handleMouseLeave = (e) => {
       setTooltipVisible(false)
-    };
-  
-    const handleMouseMove = (e) => {
-      if (isDragging) {
-        setTooltipVisible(false)
-        const newX = position.x + e.movementX
-        const newY = position.y + e.movementY
-        const maxX = largeDivRef.right  //largeDivWidth - smallDivWidth 
-        const maxY = largeDivRef.bottom  //largeDivWidth - smallDivWidth 
-        const boundX = Math.max(largeDivRef.left, Math.min(newX, maxX))   //0
-        const boundY = Math.max(largeDivRef.top, Math.min(newY, maxY))   //0
-        setPosition({ x: boundX, y: boundY });
-      }
-    };
-  
-    const handleMouseUp = (e) => {
-      setIsDragging(false);
-      let smallDivBottom = smallDivRef.current?.getBoundingClientRect().bottom
-      let smallDivTop = smallDivRef.current?.getBoundingClientRect().top
-      let smallDivLeft = smallDivRef.current?.getBoundingClientRect().left
-      let smallDivRight = smallDivRef.current?.getBoundingClientRect().right
-      let largeDivRight = largeDivRef.current?.getBoundingClientRect().right
-      let largeDivLeft = largeDivRef.current?.getBoundingClientRect().left
-      let largeDivBottom = largeDivRef.current?.getBoundingClientRect().bottom
-      let largeDivTop = largeDivRef.current?.getBoundingClientRect().top
-      if(options === "bottom") {
-        setTooltipPosition({
-          x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
-          y: smallDivBottom + tooltipHeight >= largeDivBottom ? 
-              smallDivBottom - (smallDivWidth + tooltipHeight) : 
-              smallDivBottom 
-        })
-      }
-      if(options === "top" || options === "") {
-        setTooltipPosition({
-          x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
-          y: smallDivTop - tooltipHeight <= largeDivTop ? 
-             smallDivBottom : smallDivTop - tooltipHeight
-        })
-      }
-      if(options === "right") {
-        setTooltipPosition({
-          x: smallDivRight+tooltipWidth >= largeDivRight ?
-             smallDivLeft-tooltipWidth : smallDivRight,
-          //Math.min(smallDivRight,largeDivRight-tooltipWidth),
-          y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
-        })
-      }
-      if(options === "left") {
-        setTooltipPosition({
-          x: smallDivLeft - tooltipWidth <= largeDivLeft ?
-             smallDivRight : smallDivLeft - tooltipWidth,
-          y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
-        })
-      }
-      setTooltipVisible(true)
-    };
-    const handleMouseEnter = (e) => {
-      setIsDragging(false);
-      let smallDivBottom = smallDivRef.current?.getBoundingClientRect().bottom
-      let smallDivTop = smallDivRef.current?.getBoundingClientRect().top
-      let smallDivLeft = smallDivRef.current?.getBoundingClientRect().left
-      let smallDivRight = smallDivRef.current?.getBoundingClientRect().right
-      let largeDivRight = largeDivRef.current?.getBoundingClientRect().right
-      let largeDivLeft = largeDivRef.current?.getBoundingClientRect().left
-      let largeDivBottom = largeDivRef.current?.getBoundingClientRect().bottom
-      let largeDivTop = largeDivRef.current?.getBoundingClientRect().top
-      if(options === "bottom") {
-        setTooltipPosition({
-          x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
-          y: smallDivBottom + tooltipHeight >= largeDivBottom ? 
-              smallDivBottom - (smallDivWidth + tooltipHeight) : 
-              smallDivBottom 
-        })
-      }
-      if(options === "top" || options === "") {
-        setTooltipPosition({
-          x: Math.min(smallDivLeft, largeDivRight - tooltipWidth),
-          y: smallDivTop - tooltipHeight <= largeDivTop ? 
-              smallDivBottom : smallDivTop - tooltipHeight
-        })
-      }
-      if(options === "right") {
-        setTooltipPosition({
-          x: smallDivRight+tooltipWidth >= largeDivRight ?
-             smallDivLeft-tooltipWidth : smallDivRight,
-          //Math.min(smallDivRight,largeDivRight-tooltipWidth),
-          y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
-        })
-      }
-      if(options === "left") {
-        setTooltipPosition({
-          x: smallDivLeft - tooltipWidth <= largeDivLeft ?
-             smallDivRight : smallDivLeft - tooltipWidth,
-          y: smallDivTop + (smallDivWidth/2 - tooltipHeight/2)
-        })
-      }
-      setTooltipVisible(true)
-    }
-    const handleMouseLeave = (e) => {
-        // handleMouseDown && setIsDragging(true)
-        // setIsDragging(false)
-        setTooltipVisible(false)
-    }
+  }
   return (
-    <div id="large-div" ref={smallDivRef}
-      onMouseDown={handleMouseDown} 
-        onMouseMove={handleMouseMove} 
+    <div ref={smallDivRef}
+        onMouseDown={handleMouseDown} 
+        // onMouseMove={handleMouseMove} 
         onMouseUp={handleMouseUp} 
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
